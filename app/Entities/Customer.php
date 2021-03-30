66027cbf8db7085ea68b1bd7014ce0307d109dd3
77web <info@77-web.com>
2021-03-30 23:35:23 +0900

    Customerエンティティ試しに作ってみる

---
+++ app/Entities/Customer.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Customer
+ * @ORM\Table()
+ * @ORM\Entity()
+ * @package App\Entities
+ */
+class Customer
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $googleId;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Customer
+     */
+    public function setName(string $name): Customer
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGoogleId(): string
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * @param string $googleId
+     * @return Customer
+     */
+    public function setGoogleId(string $googleId): Customer
+    {
+        $this->googleId = $googleId;
+        return $this;
+    }
+}
