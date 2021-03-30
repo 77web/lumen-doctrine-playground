faa9890bb817e5f9eeb35fde952789a4484b81ff
77web <info@77-web.com>
2021-03-30 23:58:51 +0900

    DoctrineORMを使ったレコードの保存のテスト

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Entities\Customer;
+use Doctrine\ORM\EntityManagerInterface;
+use Illuminate\Http\Response;
+use Laravel\Lumen\Http\Request;
+
 class ExampleController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
+     * @var EntityManagerInterface
      */
-    public function __construct()
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-        //
+        $this->em = $em;
     }
 
-    //
+    public function index(Request $request): Response
+    {
+        $customer = new Customer();
+        $customer
+            ->setName('dummy')
+            ->setGoogleId('dummy');
+
+        $this->em->persist($customer);
+        $this->em->flush();
+
+        return new Response('success', 200);
+    }
 }
--- tests/Feature/ExampleControllerTest.php
+++ tests/Feature/ExampleControllerTest.php
@@ -2,7 +2,20 @@
 
 namespace Tests\Feature;
 
-class ExampleControllerTest
-{
+use App\Entities\Customer;
+use Doctrine\ORM\EntityManagerInterface;
+use Tests\TestCase;
 
+class ExampleControllerTest extends TestCase
+{
+    public function test()
+    {
+        $app = $this->createApplication();
+
+        $this->get('/example-save');
+        $this->assertEquals(200, $this->response->getStatusCode());
+
+        $customer = $app->get(EntityManagerInterface::class)->find(Customer::class, 1);
+        $this->assertNotNull($customer);
+    }
 }
